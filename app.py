7023db729a51e21f15c9ed0331e294d9d1816cda
JosueArmandoRivera <125640131+JosueArmandoRivera@users.noreply.github.com>
2026-02-19 21:10:25 -0600

    Detalles 19-02-2026

--- app.py
+++ app.py
@@ -14,14 +14,36 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig) #Así se instancia a la base de datos
 csrf = CSRFProtect()
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form=forms.UserForm(request.form)
+	alumno = Alumnos.query.all()
+
+	return render_template("index.html", form=create_form,alumno=alumno)
 
 @app.route("/alumnos")
 def alumnos():
-	return render_template("alumnos.html")
+	create_form=forms.UserForm(request.form)
+	if request.method == 'POST':
+		alum=Alumnos(nombre = create_form.nombre.data, 
+			   		 apaterno = create_form.apaterno.data,
+					 email = create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("Alumnos.html", form=create_form)
+
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+	create_form=forms.UserForm(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id=request.args.get
+		
+		return redirect(url_for('/index'))
+	return render_template("detalles.html", form=create_form)
 
 @app.errorhandler(404)
 def page_not_found(e):
